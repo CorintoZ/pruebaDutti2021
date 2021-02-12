6612422d235d21d62dbe98fb776b170f59e38cae
David <david.moreno92@gmail.com>
2021-02-12 20:04:01 +0100

    Refactor - Login and Register as modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 // Components
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { RegisterComponent } from "./components/auth/register/register.component";
 
 const routes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+  },
   {
     path: "principal",
     loadChildren: () => import(`./components/principal/principal.module`).then((m) => m.PrincipalModule),
+  },
+  { path: "login", loadChildren: () => import("./components/auth/login/login.module").then((m) => m.LoginModule) },
+  {
+    path: "register",
+    loadChildren: () => import("./components/auth/register/register.module").then((m) => m.RegisterModule),
   },
   // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { PrincipalModule } from "./components/principal/principal.module";
 
 // Components
 import { AppComponent } from "./app.component";
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
 import { PrincipalComponent } from "./components/principal/principal.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, PrincipalComponent],
+  declarations: [AppComponent, PrincipalComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, PrincipalModule],
   providers: [],
   bootstrap: [AppComponent],
