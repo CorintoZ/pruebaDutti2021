4e41dcd9ccb3510339cd7dbf3e5974eff8e0a5c6
David <david.moreno92@gmail.com>
2021-02-16 11:21:51 +0100

    Refactor - Use AuthService.registration() on Register component

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 // JSON
 import usersList from "src/assets/json/users.json";
@@ -9,12 +10,13 @@ import usersList from "src/assets/json/users.json";
   selector: "app-register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.scss"],
+  providers: [AuthService],
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   dataLoading: boolean = false;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -22,18 +24,22 @@ export class RegisterComponent implements OnInit {
       last_name: ["", [Validators.required, Validators.minLength(3)]],
       username: ["", [Validators.required, Validators.minLength(3)]],
       email: ["", [Validators.required, Validators.minLength(6)]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  registerUser() {
+  async registerUser() {
     if (this.registerForm.invalid) {
       return;
     }
-    // TODO : Falta integrar el servicio para registrar al usuario
-    // JSON simulando usuarios
-    var userLogin = this.registerForm.value;
-    usersList.push(userLogin);
-    console.log("User Register -->", usersList);
-    this.router.navigate(["/principal/ships"]);
+    const { username, email, password, first_name, last_name } = this.registerForm.value;
+    try {
+      const user = this.authService.register(email, password, username, first_name, last_name);
+      if (user) {
+        this.router.navigate(["/principal/pageOne"]);
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
