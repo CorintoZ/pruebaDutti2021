a7f4aa190d3e0da5381d569b26e741b8ece8f649
David <david.moreno92@gmail.com>
2021-02-16 11:22:32 +0100

    Feature - Add log out option

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
   selector: "app-principal",
   templateUrl: "./principal.component.html",
   styleUrls: ["./principal.component.scss"],
+  providers: [AuthService],
 })
 export class PrincipalComponent implements OnInit {
-  constructor() {}
+  public isLoggedIn: boolean = false;
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  async ngOnInit() {
+    const user = await this.authService.getCurrentUser();
+    if (user) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  handleLogout() {
+    this.authService.logout();
+    this.router.navigate([""]);
+  }
 }
